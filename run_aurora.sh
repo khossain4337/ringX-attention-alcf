a22592a03a0417909544c63bb492a01241e59d20
Khalid Hossain <khossain4337@gmail.com>
2026-04-22 22:12:03 +0000

    Minor changes to run specific cases

--- run_aurora.sh
+++ run_aurora.sh
@@ -13,12 +13,28 @@ module add frameworks
 #Quick timer validation: targets the seqlen/config that showed negative elapsed_time
 #BACKEND=fused \
 #BENCHMARK_MODES="backward fwd_bwd" \
-#ALGOS="ringX_attn.ringX1_attn" \
+#ALGOS="ringX_attn.ringX1_attn ringX_attn.ringX2_attn" \
 #SEQ_LENGTHS="8192" \
 #BATCH_SIZE=2 \
-#NUM_HEADS=8 \
+#NUM_HEADS=32 \
 #NUM_ITER=3 \
+#NPROC=4 \
 #./script/run_benchmarks.sh
 
-BACKEND=fused BENCHMARK_MODES="forward backward fwd_bwd" ./script/run_benchmarks.sh
-#BACKEND=portable BENCHMARK_MODES="forward backward fwd_bwd" ./script/run_benchmarks.sh
+#BACKEND=fused BENCHMARK_MODES="forward backward fwd_bwd" ./script/run_benchmarks.sh
+
+BACKEND=fused ALGOS="ringX_attn.ringX1_attn" \
+    BENCHMARK_MODES="forward backward fwd_bwd" \
+    NPROC=4 ./script/run_benchmarks.sh
+
+BACKEND=portable ALGOS="ringX_attn.ringX1_attn" \
+    BENCHMARK_MODES="forward backward fwd_bwd" \
+    NPROC=4 ./script/run_benchmarks.sh
+
+#BACKEND=fused ALGOS="ringX_attn.ringX2_attn" \
+#    BENCHMARK_MODES="forward backward fwd_bwd" \
+#    NPROC=4 ./script/run_benchmarks.sh
+#
+#BACKEND=portable ALGOS="ringX_attn.ringX2_attn" \
+#    BENCHMARK_MODES="forward backward fwd_bwd" \
+#    NPROC=4 ./script/run_benchmarks.sh
